326a92ca111d09bd1e87dcac102f0e70f4a9abfa
pvv <pvvchip@mail.ru>
2019-06-18 00:30:55 +0300

    lesson 1

---
+++ src/main/java/ru/pvvchip/emarket/controller/LoginController.java
@@ -0,0 +1,13 @@
+package ru.pvvchip.emarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
+}
